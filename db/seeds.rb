f5c469351ffff7e6068ad33446e67837cab83e04
Robyn Jia Hui Liu <robyn.jh.liu@gmail.com>
2016-03-01 12:58:29 +0800

    ASsignment 30: Rails seeding data: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,12 @@ puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
  
+puts "Before unique post #{Post.count}"
+#Create a post with a unique title and body
+Post.find_or_create_by(title: 'a unique title', body: 'a unique body')
+puts "After unique post #{Post.count}"
+
+puts "Before unique comment #{Comment.count}"
+Comment.find_or_create_by(body: 'comment unique body')
+puts "after unique comment #{Comment.count}"
+
