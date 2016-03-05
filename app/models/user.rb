86030649a0495444f1652f01dee64b4b915e0eb2
Robyn Jia Hui Liu <robyn.jh.liu@gmail.com>
2016-03-05 21:30:13 +0800

    Assignment 36: Rails - Authentication user model : Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ActiveRecord::Base
 	before_save{self.email = email.downcase}
+	before_save :format_name
 	validates :name, length: {minimum: 1, maximum: 100}, presence: true
 	validates :password, presence: true, length: {minimum: 6}, if: "password_digest.nil?"
 	validates :password, length: {minimum: 6}, allow_blank: true
 	validates :email, presence: true, uniqueness: {case_sensitive: false}, length:{minimum: 3, maximum: 254}
 
 	has_secure_password
+end
+
+def format_name
+	if self.name
+		name_arr = []
+		self.name.split.each do |fl|
+			name_arr << fl.capitalize
+		end
+		self.name = name_arr.join(" ")
+	end
 end
