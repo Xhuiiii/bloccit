18ea51e50c06a5066b1a4d1f94760020e6e86235
Robyn Jia Hui Liu <robyn.jh.liu@gmail.com>
2016-03-08 19:57:50 +0800

    Assignment 37: Rails - Authentication Signing up : Completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   	resources :posts, except: [:index]
   end
   resources :users, only: [:new, :create]
-  
+  post 'users/confirmation' => 'users#confirmation'
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+
+  
 end
