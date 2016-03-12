f7a56f9c07e04658ecc6f740613ab452d60b7d26
Robyn Jia Hui Liu <robyn.jh.liu@gmail.com>
2016-03-12 20:48:49 +0800

    Checkpoint 43 : Rails - Voting : Completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@ Rails.application.routes.draw do
   resources :labels, only: [:show]
   resources :questions
   resources :advertisements
+  resources :sessions, only: [:new, :create, :destroy]
   resources :topics do
   	resources :posts, except: [:index]
   end
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   resources :users, only: [:new, :create]
+
   post 'users/confirmation' => 'users#confirmation'
+
   get 'about' => 'welcome#about'
   get 'faq' => 'welcome#faq'
-  resources :sessions, only: [:new, :create, :destroy]
+  
   root 'welcome#index'
 
   
